5eba8b8d2ae99c34fe6f7967e5633ebda1268558
Adam Schaefers <schaefers@riseup.net>
2022-10-08 23:05:44 -0600

    Rough draft 1

--- org/config.el
+++ org/config.el
@@ -1,32 +1,27 @@
 ;; https://orgmode.org/worg/org-tutorials/org-publish-html-tutorial.html
 (require 'ox-publish)
-
 (add-to-list 'spartan-package-list 'ox-twbs)
+(require 'ox-twbs)
 
 (setq org-publish-project-alist
       '(
         ("org-static"
          :base-directory "~/repos/cowabungadude.dev/org"
          :publishing-directory "~/repos/cowabungadude.dev"
-         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3"
          :recursive t
          :publishing-function org-publish-attachment
          )
         ("org-notes"
          :base-directory "~/repos/cowabungadude.dev/org"
          :publishing-directory "~/repos/cowabungadude.dev"
-         ;; :publishing-function org-html-publish-to-html
          :publishing-function org-twbs-publish-to-html
-         :with-sub-superscript nil
          :recursive t
-         :headline-levels 4
-         :auto-preamble t
-         :auto-postamble t
          :with-author t
-         :with-creator t
          ))
       org-html-validation-link nil
-      org-export-with-email t)
+      org-export-with-timestamps nil
+      org-export-with-email nil)
 
 ;; ;; https://systemcrafters.net/publishing-websites-with-org-mode/building-the-site/
 ;; ;; Customize the HTML output
